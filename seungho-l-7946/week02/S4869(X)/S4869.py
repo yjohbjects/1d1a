8de847cb4711dc6526450857edb152397f7cbe1b
seungho-l-7946 <109324469+seungho-l-7946@users.noreply.github.com>
2022-08-13 09:51:16 +0900

    Add files via upload

---
+++ seungho-l-7946/week02/S4869(X)/S4869.py
@@ -0,0 +1,11 @@
+# 종이붙이기
+import sys
+sys.stdin = open("onedayonealgorithm/week02/S4869/input.txt")
+
+num = int(input())
+
+for i in range(1, num+1):
+
+    result = 0
+    
+    print(f'#{i} {result}')
